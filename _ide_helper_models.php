a069d3c2ddb8fdb520f92a6d2f11767ed8592109
Mauro Campos <mauro.pcsergio@gmail.com>
2021-04-06 09:27:56 +0000

    ide helper

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -12,6 +12,113 @@
 
 namespace App\Models{
 /**
+ * App\Models\Category
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Category newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Category newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Category query()
+ */
+	class Category extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Client
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $client_category_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Client newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Client newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Client onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Client query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereClientCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|Client withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Client withoutTrashed()
+ */
+	class Client extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\ClientCategory
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $deleted_at
+ * @method static \Illuminate\Database\Eloquent\Builder|ClientCategory newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ClientCategory newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ClientCategory query()
+ * @method static \Illuminate\Database\Eloquent\Builder|ClientCategory whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ClientCategory whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ClientCategory whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ClientCategory whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ClientCategory whereUpdatedAt($value)
+ */
+	class ClientCategory extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Equipaments
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $deleted_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Equipaments newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Equipaments newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Equipaments query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Equipaments whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Equipaments whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Equipaments whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Equipaments whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Equipaments whereUpdatedAt($value)
+ */
+	class Equipaments extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Loan
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $client_id
+ * @property int $equipament_id
+ * @property string $due_date
+ * @property string|null $return_date
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read mixed $status
+ * @method static \Illuminate\Database\Eloquent\Builder|Loan newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Loan newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Loan query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Loan whereClientId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Loan whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Loan whereDueDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Loan whereEquipamentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Loan whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Loan whereReturnDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Loan whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Loan whereUserId($value)
+ */
+	class Loan extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
  * App\Models\User
  *
  * @property int $id
@@ -26,6 +133,7 @@ namespace App\Models{
  * @property string|null $profile_photo_path
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $deleted_at
  * @property-read string $profile_photo_url
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
  * @property-read int|null $notifications_count
@@ -43,6 +151,7 @@ namespace App\Models{
  * @method static \Illuminate\Database\Eloquent\Builder|User role($roles, $guard = null)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereCurrentTeamId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereDeletedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
